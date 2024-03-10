a24e4a8760989b663374608e7e3835bdc1fb9f04
toasterheld <98853022+toasterheld@users.noreply.github.com>
2024-03-10 03:39:13 +0100

    creates landingpage.php

---
+++ LandingPage.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faive | Das Familien-HQ</title>
+    <link rel="icon" href="../assets/logos/faive_transparent.ico">
+    <link rel="stylesheet" href="css/import.css">
+</head>
+<body>
+    <div class="content">
+        <header>
+            <span><img src="assets/logos/faive_transparent.png" class="logo"></span>
+            <nav class="navbar">
+                <div class="navbar_links">
+                    <a href="#">Funktionen</a>
+                    <a href="#">Fragen</a>
+                    <a href="#">Familiäres</a>
+                </div>
+            </nav>
+            <span><button class="start_into">Starte jetzt</button></span>
+        </header>
+        <div id="page_intro">
+            <div id="focus_text">
+                <p>Die <span style="color: #008359;">Familien-App</span></p>
+                </p>der nächsten Generation</p>
+            </div>
+            <div id="intro_text">
+                <p>Organisiere zukünftige Aktivitäten, teile Erinnerungen und verteile die</p>
+                <p>wichtigsten Aufgaben im Haushalt – alles an einem zentralen Ort</p>
+            </div>
+            <div id="action_buttons">
+                <button class="blacked_start">Starte jetzt <span style="margin-left: 10px" class="material-symbols-outlined">trending_flat</span></button>
+                <button class="learn_more">So funktioniert´s <span style="margin-right: 10px" class="material-symbols-outlined">help</span></button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
